179c31ad52f98336bea35cf233911551b88abe87
Károly Zsolt Krekács <krekacs.k@outlook.com>
2018-07-19 22:26:29 +0200

    Completed Task1: Sort Objects by cost_in_credits using adv bubblesort.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+function getIntPropertyValue(parameterProperty) {
+  if (typeof parameterProperty === 'string') {
+    return parseInt(parameterProperty, 10);
+  }
+
+  return 0;
+}
+
+function doSortObjectsByCost(parameterArray) {
+  var endIndex = parameterArray.length - 1;
+  var newEndIndex;
+  while (endIndex > 1) {
+    newEndIndex = 0;
+    for (var i = 0; i < endIndex; i++) {
+      if (getIntPropertyValue(parameterArray[i].cost_in_credits) >
+            getIntPropertyValue(parameterArray[i + 1].cost_in_credits)) {
+        [parameterArray[i], parameterArray[i + 1 ]] = [parameterArray[i + 1], parameterArray[i]];
+        newEndIndex = i;
+      }
+    }
+    endIndex = newEndIndex;
+  }
+}
+
 function getData(url, callbackFunc) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -5,14 +29,15 @@ function getData(url, callbackFunc) {
       callbackFunc(this);
     }
   };
-  xhttp.open("GET", url, true);
+  xhttp.open('GET', url, true);
   xhttp.send();
 }
 
 function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
-  
+  doSortObjectsByCost(userDatas);
+  console.log(userDatas);
 }
 
 getData('/json/spaceships.json', successAjax);
